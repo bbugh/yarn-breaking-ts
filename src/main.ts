fa6c0a77ef33d4430f0f3b825b1a9fe33742a1cd
Brian Bugh <438465+bbugh@users.noreply.github.com>
2021-06-30 23:35:01 -0500

    Breaking change

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createI18n } from "vue-i18n";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const I18n = createI18n({
+  legacy: false,
+  locale: localStorage.getItem("lang") || "en",
+  fallbackLocale: "en",
+  messages: {
+    en: {
+      hello: "Hello world",
+      tos: "vue-i18n-next docs",
+      term: "access to {0}",
+    },
+  },
+});
+
+createApp(App).use(I18n).mount("#app");
